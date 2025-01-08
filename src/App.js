69fa828dc2156fd22028af4fc3481048af3128c6
Isabel Tiburski <itiburski@gmail.com>
2025-01-08 11:36:57 -0300

    change message text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          docker-course-ci-cd-workflow example
         </p>
         <a
           className="App-link"
